ca797a3ede1217fd81bcf5f1db9956d37d56569f
Rick <90382366+rickcain@users.noreply.github.com>
2023-07-26 15:04:12 +0800

    feature: add Optimism UniswapV3 aggregator

---
+++ contracts/aggregators/Optimism/OMOOptimismUniswapV3Aggregator.sol
@@ -0,0 +1,154 @@
+// SPDX-License-Identifier: AGPL-3.0
+
+pragma solidity ^0.8.0;
+
+import "../../access/Ownable.sol";
+import "../../interfaces/IBridge.sol";
+import "../../assets/interfaces/IWETH.sol";
+import "../interfaces/IUniswapV3SwapRouter.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract OMOOptimismUniswapV3Aggregator is Ownable {
+    using SafeERC20 for IERC20;
+
+    event LOG_AGG_SWAP (
+        address caller,
+        uint256 amountIn,
+        address tokenIn,
+        uint256 amountOut,
+        address tokenOut,
+        address receiver,
+        uint256 fee
+    );
+
+    address public WETH = 0x4200000000000000000000000000000000000006;
+    address public router = 0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45;
+    address public bridge = 0x0000000000000000000000000000000000000000;
+    address public feeCollector;
+
+    constructor (address _feeCollector) {
+        require(_feeCollector != address(0), "feeCollector address cannot be zero");
+        feeCollector = _feeCollector;
+    }
+
+    receive() external payable { }
+
+    function exactInputSingle(
+        IUniswapV3SwapRouter.ExactInputSingleParams memory params,
+        bool unwrapETH
+    ) external payable {
+        _pull(params.tokenIn, params.amountIn, 0);
+
+        (uint amountOut, uint feeAmount, address receiver) = _swap(params, msg.value > 0, unwrapETH, params.recipient);
+
+        if (unwrapETH) {
+            require(params.tokenOut == WETH, 'OMOAggregator: INVALID_TOKEN_OUT');
+
+            IWETH(WETH).withdraw(amountOut);
+
+            _sendETH(receiver, amountOut - feeAmount);
+            _sendETH(feeCollector, feeAmount);
+        } else {
+            IERC20(params.tokenOut).safeTransfer(receiver, amountOut - feeAmount);
+            IERC20(params.tokenOut).safeTransfer(feeCollector, feeAmount);
+        }
+    }
+
+    function exactInputSingleCrossChain(
+        IUniswapV3SwapRouter.ExactInputSingleParams memory params,
+        uint netFee, uint32 destinationDomain, bytes32 recipient, bytes memory callData // args for bridge
+    ) external payable {
+        _pull(params.tokenIn, params.amountIn, netFee);
+
+        (uint amountOut, uint feeAmount, ) = _swap(params, msg.value > netFee, false, msg.sender);
+        IERC20(params.tokenOut).safeTransfer(feeCollector, feeAmount);
+        uint bridgeAmount = amountOut - feeAmount;
+
+        IERC20(params.tokenOut).safeApprove(bridge, bridgeAmount);
+
+        IBridge(bridge).bridgeOut{value: netFee}(
+            params.tokenOut,
+            bridgeAmount,
+            destinationDomain,
+            recipient,
+            callData
+        );
+    }
+
+    function _pull(address token, uint amount, uint netFee) internal {
+        require(msg.value >= netFee, "OMOAggregator: invalid netFee");
+
+        if (msg.value > netFee) {
+            require(token == WETH, 'OMOAggregator: INVALID_TOKEN_IN');
+            IWETH(WETH).deposit{value: msg.value - netFee}();
+        } else {
+            require(amount > 0, 'OMOAggregator: INSUFFICIENT_INPUT_AMOUNT');
+            IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
+        }
+    }
+
+    function _swap(
+        IUniswapV3SwapRouter.ExactInputSingleParams memory params,
+        bool nativeIn, bool nativeOut, address logReceiver
+    ) internal returns (uint, uint, address) {
+        require(params.recipient != address(0), 'OMOAggregator: INVALID_RECIPIENT');
+        address receiver = params.recipient;
+        params.recipient = address(this);
+
+        IERC20(params.tokenIn).safeApprove(router, params.amountIn);
+
+        uint balanceBefore = IERC20(params.tokenOut).balanceOf(address(this));
+        IUniswapV3SwapRouter(router).exactInputSingle(params);
+        uint amountOut = IERC20(params.tokenOut).balanceOf(address(this)) - balanceBefore;
+        uint feeAmount = amountOut * params.fee / 1000000;
+
+        address tokenIn = params.tokenIn;
+        if (nativeIn) tokenIn = address(0);
+        address tokenOut = params.tokenOut;
+        if (nativeOut) tokenOut = address(0);
+
+        emit LOG_AGG_SWAP(
+            msg.sender,
+            params.amountIn,
+            tokenIn,
+            amountOut,
+            tokenOut,
+            logReceiver,
+            feeAmount
+        );
+
+        return (amountOut, feeAmount, receiver);
+    }
+
+    function _sendETH(address to, uint256 amount) internal {
+        (bool success,) = to.call{value:amount}(new bytes(0));
+        require(success, 'OMOAggregator: ETH_TRANSFER_FAILED');
+    }
+
+    function setWETH(address _weth) external onlyOwner {
+        WETH = _weth;
+    }
+
+    function setRouter(address _router) external onlyOwner {
+        require(_router != address(0), "router address cannot be zero");
+        router = _router;
+    }
+
+    function setBridge(address _bridge) external onlyOwner {
+        require(_bridge != address(0), "bridge address cannot be zero");
+        bridge = _bridge;
+    }
+
+    function setFeeCollector(address _feeCollector) external onlyOwner {
+        feeCollector = _feeCollector;
+    }
+
+    function rescueFund(address tokenAddress) external onlyOwner {
+        IERC20 token = IERC20(tokenAddress);
+        if (tokenAddress == WETH && address(this).balance > 0) {
+            _sendETH(msg.sender, address(this).balance);
+        }
+        token.safeTransfer(msg.sender, token.balanceOf(address(this)));
+    }
+}
