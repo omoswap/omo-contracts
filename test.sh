763a4ebc5099f800425626ce2f9933aca9c079d2
Rick <90382366+rickcain@users.noreply.github.com>
2023-10-10 18:22:20 +0800

    test: update test shell

--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
 sourceHash="96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f"
-testHash="badcace35eca3975b86a7d744a198f996983b3f8f265644b0c0db50289070a24"
+
+testHash=$( npx hardhat test ./test/utils/init.ts )
+testHash=${testHash#*0x}
+testHash=${testHash:0:64}
+
 file="$1"
 
 sed -i '' -e "s/$sourceHash/$testHash/" contracts/aggregators/Ethereum/libraries/EthereumUniswapV2Library.sol
