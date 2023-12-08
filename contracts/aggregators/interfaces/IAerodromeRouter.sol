06fbbc40881624bec7df0234bb0ada3db371180c
Rick <90382366+rickcain@users.noreply.github.com>
2023-12-08 18:05:49 +0800

    feature: add Base Aerodrome aggregator

---
+++ contracts/aggregators/interfaces/IAerodromeRouter.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: AGPL-3.0
+
+pragma solidity >=0.8.0;
+
+interface IAerodromeRouter {
+    struct route {
+        address from;
+        address to;
+        bool stable;
+        address factory;
+    }
+
+    function swapExactTokensForTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        route[] calldata routes,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+
+    function swapExactETHForTokens(
+        uint256 amountOutMin,
+        route[] calldata routes,
+        address to,
+        uint256 deadline
+    ) external payable returns (uint256[] memory amounts);
+
+    function swapExactTokensForETH(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        route[] calldata routes,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+}
