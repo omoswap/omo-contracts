f4c46aebeaa6017669a9b4c7bf721de6aeb89073
Rick <90382366+rickcain@users.noreply.github.com>
2024-04-18 16:53:36 +0800

    feature: add BNBChain Ellipsis aggregator

--- contracts/aggregators/interfaces/ICurve.sol
+++ contracts/aggregators/interfaces/ICurve.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0
 
-pragma solidity >=0.8.0;
+pragma solidity ^0.8.0;
 
 interface ICurve {
     function exchange(int128 i, int128 j, uint256 dx, uint256 minDy) external returns (uint256 dy);
